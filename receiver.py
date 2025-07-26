834ad6a3b445ecab632bb9bfc1a944510252b9e1
oftx <3204417902@qq.com>
2025-07-27 02:28:43 +0800

    Update files

--- receiver.py
+++ receiver.py
@@ -1,5 +1,12 @@
 from tmux_cmd_runner import TmuxTerminal, CommandResult
 
+# def send_keys(keys: str, term: TmuxTerminal):
+#     term._pane.send_keys(keys)
+
+# def send_keys_batch(keys_list: list | tuple, term: TmuxTerminal):
+#     for keys in keys_list:
+#         term._pane.send_keys(keys)
+
 with TmuxTerminal(session_name="hello") as term:
     term.execute(
         [
