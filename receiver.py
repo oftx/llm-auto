ab8963789840f09891f4a66043aaddfbf7388931
oftx <3204417902@qq.com>
2025-07-27 02:44:12 +0800

    Update files

--- receiver.py
+++ receiver.py
@@ -10,7 +10,7 @@ from tmux_cmd_runner import TmuxTerminal, CommandResult
 with TmuxTerminal(session_name="hello") as term:
     term.execute(
         [
-            "date"
+            "seq 10"
         ]
     )
     print(CommandResult.get())
